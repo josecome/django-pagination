efbda27c97ca37a3ef504a669d83c58f597ed1d2
Jose Jaime Come <josejaimecome@gmail.com>
2024-12-09 22:51:04 +0200

    Gitignore file added

--- app/views.py
+++ app/views.py
@@ -11,8 +11,7 @@ def home_view(request):
     paginator = Paginator(posts, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    return render(request, 'home.html', 
-                  {'page_obj': page_obj }
-                  )
+
+    return render(request, 'home.html', {'page_obj': page_obj })
 
     
